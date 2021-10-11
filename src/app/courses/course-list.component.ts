fb5d10513e8f36b6bf1c18a3479e9e869959a58e
ronsv <ronie.souzavieira@gmail.com>
2021-10-11 10:06:37 -0300

    injecao de dependencia

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from './course';
+import { CourseService } from './course.service';
 
 @Component({
     selector: 'app-course-list',
@@ -7,28 +8,8 @@ import {Course} from './course';
 })
 export class CourseListComponent implements OnInit{
     courses: Course[] = [];
+    constructor(private courseService: CourseService){}
     ngOnInit(): void{
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '',
-                price:  99.99,
-                code: 'XPS-8796',
-                duration: 120,
-                rating: 5.4,
-                releaseDate: 'November, 2, 2021'
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: '',
-                price:  45.99,
-                code: 'LKS-8891',
-                duration: 80,
-                rating: 4,
-                releaseDate: 'November, 4, 2021'
-            }
-        ]
+        this.courses = this.courseService.retrieveAll();
     }
 }  
